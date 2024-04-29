a2425af8535a3c37e63a1217259bddfaa4b436c0
zhl <1127287255@qq.com>
2024-04-29 13:51:44 +0800

    0429 dtu geo conf fix 1

--- geo/NeuS-ours2/models/helpers.py
+++ geo/NeuS-ours2/models/helpers.py
@@ -14,9 +14,9 @@ conf_dict = \
      'colmap_bottle': './confs/wmask.conf',
      'colmap_tools2': './confs/wmask.conf',
      'colmap_wshoes': './confs/wmask.conf',
-     'dtu_scan24': './confs/nerf.conf',
-     'dtu_scan69': './confs/nerf.conf',
-     'dtu_scan110': './confs/nerf.conf',}
+     'dtu_scan24': './confs/dtu.conf',
+     'dtu_scan69': './confs/dtu.conf',
+     'dtu_scan110': './confs/dtu.conf',}
 
 cg_data_list = ['drums_3072', 'lego_3072', 'hotdog_2163', 'materials_2163', 'ficus_2188',
                 'chair0_3072', 'machine1_3072', 'kitchen6_7095']
